bcdfea15caeaeb47ae881fde39ca4eebfebaad82
Vitória Mendes <vitoria.luizanm@gmail.com>
2026-04-05 03:31:47 -0300

    primeiro commit - Ex01CotacaoDolar

---
+++ src/br/com/vitoria/exercicios/Ex00HelloWorld.java
@@ -0,0 +1,9 @@
+package br.com.vitoria.exercicios;
+
+public class Ex00HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("        === Estudando Java ===");
+        System.out.println("Bem vindo aos meus exercicios basicos!");
+    }
+}
